ff418a6915b6cfa0d057284ec7b2c2e4c99f5164
Augustika <augustika1524@gmail.com>
2024-10-18 14:25:16 +0530

    initial-commit

---
+++ client/src/components/Services.jsx
@@ -0,0 +1,153 @@
+import Section from "./Section";
+import Heading from "./Heading";
+import { service1, service2,  check } from "../assets";
+import image from "../assets/image.png"
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import {
+  
+  Gradient,
+ 
+} from "./design/Services";
+import logo  from '../assets/logo.png';
+import Generating from "./Generating";
+import chatbotdemo from '../assets/collaboration/chatbot-demo.mp4' ;
+import converse from '../assets/collaboration/converse.mp4'
+
+
+const Services = () => {
+  return (
+    <Section id="how-to-use">
+      <div className="container">
+        <Heading
+         
+          text="Yugm : Revolutionizing Knowledge Management with AI driven Accessibility & Security"
+        />
+
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[40rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+              <img
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                alt="Yugm AI"
+                height={730}
+                src={service1}
+              />
+            </div>
+
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+            <div className="flex justify-start items-center">
+            <img src={logo} width={100} height={20} alt="Yugm" />
+             <h4 className="h4">Yugm AI</h4>
+ 
+            </div>
+
+              <p className="body-2 mb-[3rem] text-n-3 text-justify">
+              The term "Yugm" meaning "pair" or "union" symbolizes the collaboration between employees and the AI."Yugm AI" reflects a modern approach to employee engagement and support, promoting a culture of collaboration and shared knowledge within the organization.
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img width={24} height={24} src={check} />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" />
+          </div>
+          <br/>
+          <br/>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2" id="demo">
+            <div className="relative min-h-[35rem] min-w-[48rem] border border-n-1/10 rounded-3xl overflow-hidden">
+             
+
+              <div className="absolute inset-0 flex flex-col justify-end align-middle p-4 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+
+                {/* < className="h4 mb-4"> */}
+                <div className="absolute inset-0 flex items-center justify-center p-3">
+                <video
+                  src={chatbotdemo}
+                  className=" object-cover  "
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  controls={false} // Remove controls if you want it to autoplay seamlessly
+                
+                />
+                </div>
+               
+              </div>
+
+              {/* <PhotoChatMessage /> */}
+            </div>
+
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[26rem] ml-[130px]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4 p-4 text-justify">
+                Streamline knowledge access across the entire organization.</h4>
+                <p className="justify-center align-middle p-4 text-justify">
+                Yugm AI consolidates knowledge access by connecting with commonly-used knowledge bases, enabling employees to obtain tailored, precise, and current information with brief responses. Additionally, it recognizes knowledge gaps and takes the initiative to develop useful content to avoid recurring problems.
+                </p>
+               
+              </div>
+
+           
+            </div>
+          </div>
+          
+          <br/>
+          <br/>
+          {/* second video bar */}
+        
+          <div className="relative z-1 grid gap-3 lg:grid-cols-2">
+        
+
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[26rem] w-[500px]">
+              <div className="py-12 px-4 xl:px-8 ">
+                <h4 className="h4 mb-4 p-4">
+                Simplifying Knowledge Retrieval</h4>
+                <p className="justify-center align-middle p-4 text-justify">
+                Yugm AI empowers users and employees by providing instant access to a centralized knowledge base. With natural language queries, it delivers accurate, concise responses, ensuring quick resolutions and enhancing productivity across the organization.
+                </p>
+               
+              </div>
+
+           
+            </div>
+            <div className="relative min-h-[35rem] min-w-[48rem] -ml-[130px] border border-n-1/10 rounded-3xl overflow-hidden ">
+             
+
+             <div className="absolute inset-0 flex flex-col justify-end align-middle p-4 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15 ">
+
+               {/* < className="h4 mb-4"> */}
+               <div className="absolute inset-0 flex items-center justify-center p-3">
+               <video
+                 src={converse}
+                 className=" object-cover  "
+                 autoPlay
+                 loop
+                 muted
+                 playsInline
+                 controls={false} // Remove controls if you want it to autoplay seamlessly
+               
+               />
+               </div>
+              
+             </div>
+           </div>
+          </div>
+          <Gradient />
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Services;
---
+++ client/src/constants/index.js
@@ -0,0 +1,208 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+ 
+} from "../assets";
+
+import msteams from '../assets/collaboration/msteams.png'
+
+
+import greythr from '../assets/collaboration/greythr.png'
+import bamboohr from '../assets/collaboration/bamboohr.png'
+
+
+export const navigation = [
+  {
+    id: "0",
+    title: "Features",
+    url: "#features",
+  },
+
+  {
+    id: "2",
+    title: "At a Glance",
+    url: "#demo",
+  },
+  {
+    id: "3",
+    title: "Coming soon",
+    url: "#coming-soon",
+  }
+
+  // {
+  //   id: "4",
+  //   title: "New account",
+  //   url: "#signup",
+  //   onlyMobile: true,
+  // },
+  // {
+  //   id: "5",
+  //   title: "Sign in",
+  //   url: "#login",
+  //   onlyMobile: true,
+  // },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+// export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+
+];
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+
+export const collabText1 =
+  "Connect your favorite tools and platforms effortlessly, making collaboration smoother and more productive than ever before";
+  export const collabText2 =
+  "Leverage AI to categorize and search information based on user roles, making information retrieval smarter, faster, and more secure. Ensure the right people access the right information at the right time, enhancing productivity and collaboration across the organization";
+
+export const collabContent = [
+  {
+    id: "0",
+    title: "Seamless Integration:",
+    text: collabText1,
+  },
+  {
+    id: "2",
+    title: "Supercharge Information Retrieval with Role-Based Access",
+    text: collabText2,
+  },
+];
+
+export const collabApps = [
+
+  {
+    id: "1",
+    title: "Microsoft Teams",
+    icon: msteams,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "GreytHR",
+    icon: greythr,
+    width: 36,
+    height: 38,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+
+  {
+    id: "5",
+    title: "Bamboo hr",
+    icon: bamboohr,
+    width: 36,
+    height: 38,
+  },
+ 
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "On-Premises Data Security",
+    text: "Control your data. Yugm's on-premises solution ensures complete data security and compliance with data sovereignty requirements.",
+    backgroundUrl: "./client/src/assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+    imageUrl: benefitImage2,
+  },
+  {
+    id: "1",
+    title: "AI-Powered Knowledge Hub",
+    text: "Automate information categorization and leverage AI for contextual search and insights generation from internal data, streamlining knowledge access across the organization.",
+    backgroundUrl: "./client/src/assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    imageUrl: benefitImage2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Conversational Knowledge retrieval",
+    text: "Equip employees with quick and efficient access to internal knowledge, enhancing business operations through conversational retrieval of crucial information",
+    backgroundUrl: "./client/src/assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+    imageUrl: benefitImage2,
+  },
+  
+
+
+];
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "#",
+  },
+];
